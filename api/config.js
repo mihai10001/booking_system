9a601dc6e7f3fe6bc131d397cca7b74dc4c515f2
mihai10001 <rvivits@gmail.com>
2021-05-14 18:17:44 +0300

    add config with .env

---
+++ api/config.js
@@ -0,0 +1,14 @@
+// Save config. variables here
+// Don't share those! Don't make them public on GitHub, etc.
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbUri = process.env.DB_URI;
+const secretSigningToken = process.env.JWT_SECRET_KEY;
+const bcryptSaltRounds = 10;
+const defaultUsers = JSON.parse(process.env.DEFAULT_USERS_ARRAY);
+
+exports.dbUri = dbUri;
+exports.secretKey = secretSigningToken;
+exports.bcryptSaltRounds = bcryptSaltRounds;
+exports.defaultUsers = defaultUsers;
