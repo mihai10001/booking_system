ec3fd121ab717b9bbb7dc140e61b32144fc3cf95
mihai10001 <rvivits@gmail.com>
2021-05-17 21:09:44 +0300

    generic JWT verification middleware

---
+++ api/middlewares/verifyJWT.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const secretKey = require('../config').secretKey;
+
+let verifyJWT = (req, res, next) => {
+  // Get the token from headers, and remove Bearer if found
+  let token = req.headers['x-access-token'] || req.headers['authorization'] || '';
+  token = token.replace(/^Bearer\s+/, '');
+  
+  if (!token)
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+    
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err)
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    else {
+      // At this point, we are authenticated
+      next();
+    }
+  });
+}
+
+exports.verifyJWT = verifyJWT;
