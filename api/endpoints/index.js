ee11975f7584f8f5d22b564b9fa9a6311c6f294d
mihai10001 <rvivits@gmail.com>
2021-05-25 18:40:10 +0300

    define endpoints for tickets controller

--- api/endpoints/index.js
+++ api/endpoints/index.js
@@ -3,6 +3,7 @@ const verifyAdminMiddleware = require('../middlewares/verifyAdminRole').verifyAd
 
 const { login, register } = require('../controllers/users');
 const { getEvents, getEvent, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+const { getTickets, getTicket, createTicket, updateTicket, deleteTicket } = require('../controllers/tickets');
 
 module.exports = function(app, dbClient) {
 
@@ -37,6 +38,29 @@ module.exports = function(app, dbClient) {
 
     app.delete('/events/:id', verifyJWTMiddleware, verifyAdminMiddleware, (req, res) => {
       deleteEvent(req, res, dbClient);
+    });
+
+
+    // Tickets
+
+    app.get('/tickets/from_event/:eventId', verifyJWTMiddleware, (req, res) => {
+      getTickets(req, res, dbClient);
+    });
+
+    app.get('/tickets/:ticketId', verifyJWTMiddleware, (req, res) => {
+      getTicket(req, res, dbClient);
+    });
+
+    app.post('/tickets/from_event/:eventId', verifyJWTMiddleware, verifyAdminMiddleware, (req, res) => {
+      createTicket(req, res, dbClient);
+    });
+
+    app.put('/tickets/:ticketId', verifyJWTMiddleware, verifyAdminMiddleware, (req, res) => {
+      updateTicket(req, res, dbClient);
+    });
+
+    app.delete('/tickets/:ticketId', verifyJWTMiddleware, verifyAdminMiddleware, (req, res) => {
+      deleteTicket(req, res, dbClient);
     });
 
 };
