c4d024683b36de3a339b52e0bda571406fe4d4e8
mihai10001 <rvivits@gmail.com>
2021-06-14 20:51:36 +0300

    add auth guard to all routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -9,11 +10,17 @@ const routes: Routes = [
     path: 'login',
     component: AuthComponent,
   },
+  {
+    path: '',
+    loadChildren: () => import('./main-layout/main-layout.module').then(m => m.MainLayoutModule),
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard]
+  },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], // , { enableTracing: true }
+  imports: [RouterModule.forRoot(routes, {useHash: true})], // , { enableTracing: true }
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
