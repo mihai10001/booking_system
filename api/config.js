4584134f683bfeaa0d80d758bb46fc156f1c9cda
mihai10001 <rvivits@gmail.com>
2021-08-12 15:26:42 +0300

    remove .env from gitignore as an example

--- api/config.js
+++ api/config.js
@@ -7,8 +7,12 @@ const dbUri = process.env.DB_URI;
 const secretSigningToken = process.env.JWT_SECRET_KEY;
 const bcryptSaltRounds = 10;
 const defaultUsers = JSON.parse(process.env.DEFAULT_USERS_ARRAY);
+const emailUser = process.env.EMAIL_USER;
+const emailPassword = process.env.EMAIL_PASSWORD;
 
 exports.dbUri = dbUri;
 exports.secretKey = secretSigningToken;
 exports.bcryptSaltRounds = bcryptSaltRounds;
 exports.defaultUsers = defaultUsers;
+exports.emailUser = emailUser;
+exports.emailPassword = emailPassword;
